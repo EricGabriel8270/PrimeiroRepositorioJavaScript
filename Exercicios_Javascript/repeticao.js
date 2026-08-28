f501d057687eae85066b4e2701ff8236c71e1539
unknown <46881046855@info.local>
2026-08-28 17:55:38 -0300

    atualizado

--- Exercicios_Javascript/repeticao.js
+++ Exercicios_Javascript/repeticao.js
@@ -95,7 +95,7 @@ do {
 }while(num > 5 )   
 */
 //ADIVINHE O NUMERO
-
+/*
 //1 a 100.
 const numeroSecreto = Math.floor(Math.random()* 10) + 1;
 
@@ -124,3 +124,200 @@ do{
         console.log("Muito alto tente um numero maior")
     }
 }while(palpite != numeroSecreto)
+*/
+
+//FOR - LOOP
+
+//let conta = 0;Variavel de controle.
+//conta < 10 - analise de condicao
+//conta++ - o que acontece com a variavel de controle.
+
+for( let conta = 0;conta < 10; conta++){
+    console.log(conta)
+}
+
+console.log("O loop terminou.")
+
+//tabuada 
+/*
+const numero = Number(prompt("Tabuada de qual numero"))
+
+if(Number.isNaN(numero)){
+    console.log("❌numero invalido")
+}
+else{
+    //console.log("TABUADA DO" + numero)
+    console.log(`\n TABUADA DO ${numero}  \n`)
+
+    for(let i =1;i <=10; i++ ){
+        const resultado = numero * i
+        //console.log(numero + "X" + i + "=" + resultado)
+        console.log(`${numero} x ${i} = ${resultado}`)
+    }
+
+}
+*/
+//Exiba numero pares de 0 a 20
+
+console.log("Numero pares de 0 a 20: \n")
+
+for(let pares= 0; pares <=20; pares+=2){
+    console.log(pares)
+
+}
+
+//Contagem regressiva com for
+
+console.log("Lancamento em : \n")
+
+for(let conta = 10; conta >= 1; conta-- ){
+    console.log(conta + "....")
+}
+
+console.log("Decolar!")
+
+//DESAFIO
+/*
+Crie um programa que mostre os numeros de 1 a 3.
+cada numero deve aparecer 3 vezes na mesma linha.
+
+saida esperada:
+111
+222
+333
+
+DICA:
+use um for para controlar os numeros
+e o outro for dentro para repetir cada numero.
+*/
+/*
+for(let cont = 1; cont <=3;cont ++){
+    let resultado =""
+    for(let numero = 1; numero <=3; numero ++) {
+        resultado += cont + ""
+    }  
+    console.log(resultado)
+}
+*/
+//linha de piramide de asteriscos
+/*
+for(let cont = 1; cont <=10;cont ++){
+    let resultado ="";
+
+    for (let espaco = 10; espaco > cont; espaco--) {
+        resultado += "";
+    }
+    for (let numero = 1; numero <= (2 * cont - 1); numero++) {
+        resultado += "*";
+    }
+    console.log(resultado)
+}
+*/
+//Exercicio 1
+/*
+for( let conta = 0;conta < 11; conta++){
+    console.log(conta)
+}
+*/
+//Exercicio 2
+/*
+const numero = Number(prompt("Tabuada de qual numero"))
+
+if(Number.isNaN(numero)){
+    console.log("❌numero invalido")
+}
+else{
+    //console.log("TABUADA DO" + numero)
+    console.log(`\n TABUADA DO ${numero}  \n`)
+
+    for(let i =1;i <=10; i++ ){
+        const resultado = numero * i
+        //console.log(numero + "X" + i + "=" + resultado)
+        console.log(`${numero} x ${i} = ${resultado}`)
+    }
+
+}
+*/
+//Exercicio 3
+/*
+const numero = Number(prompt("Qual e o valor?"))
+let resultado = 0
+let expressao = ""
+for(let i = 1; i<= numero; i++){
+    resultado=resultado + i 
+    expressao+=i
+
+    if (i< numero){
+        expressao += "+ "
+    }
+}
+console.log(`${expressao} = ${resultado}`)
+*/
+//Exercicio 4
+/*
+console.log("Numero pares de 0 a 20: \n")
+
+for(let pares= 0; pares <=50; pares+=2){
+    console.log(pares)
+
+}
+*/
+//Exercicio 5
+/*
+const numeroSecreto = Math.floor(Math.random()* 100) + 1;
+
+let palpite;
+let tentativas = 0;
+
+console.log("Adivinhe o numero etre 1 e 100.")
+console.log(numeroSecreto)
+
+do{
+    //alert( Math.floor(Math.random()* 10) + 1)
+
+    palpite = Number(prompt("Seu palpite"))
+    tentativas++
+
+    if(Number.isNaN(palpite)){
+        console.log("Digite um numero valido")
+    }
+
+    else if (palpite == numeroSecreto){
+        console.log("Acertou" + "Em" + tentativas + "tentativas.")
+    }
+    else if (palpite < numeroSecreto){
+        console.log("Muito baixo tente um numero menor")
+    }
+    else if (palpite > numeroSecreto){
+        console.log("Muito alto tente um numero maior")
+    }
+}while(palpite != numeroSecreto)
+*/
+//Exercicio 6
+/*
+console.log("Lancamento em : \n")
+
+for(let conta = 5; conta >= 1; conta-- ){
+    console.log(conta + "....")
+}
+
+console.log("Decolar!")
+*/
+//Exercicio 7
+const numero = Number(prompt("Qual e o valor?"))
+let resultado = 0
+let expressao = ""
+    for(let i = 1; i<= numero; i++){
+        resultado=resultado + i 
+        expressao+=i
+
+        if (i< numero){
+            expressao += "+ "
+        }
+    }
+
+console.log(`${expressao} = ${resultado}`)
+
+
+
+
